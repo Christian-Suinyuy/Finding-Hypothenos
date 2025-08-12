3e6c7e61d29e8ce4127c4563cc18f210f0608980
christian <christiansuinyuy@gmal.com>
2025-08-12 14:48:46 +0100

    added space when displayin results

--- hypotenuse.js
+++ hypotenuse.js
@@ -7,6 +7,6 @@ buttonElem.addEventListener('click', ()=>{
     let a = Number(document.getElementById('a').value)
     let b = Number(document.getElementById('b').value)
     let hyp = Math.sqrt((a*a)+(b*b))
-    document.querySelector('h2').innerText += hyp
+    document.querySelector('h2').innerText += ` ${hyp}`
 })
 
